55d8a22219a79db7de8e8e3d24b05cafa3266eaf
Martinus <martinus.molloy@gmail.com>
2020-04-09 09:46:16 +0000

    Corrected Z probe offset height.

--- Configuration.h
+++ Configuration.h
@@ -966,7 +966,7 @@
  *
  * Specify a Probe position as { X, Y, Z }
  */
-#define NOZZLE_TO_PROBE_OFFSET { -45, 0, -0.85 }
+#define NOZZLE_TO_PROBE_OFFSET { -45, 0, -1.67 }
 
 // Most probes should stay away from the edges of the bed, but
 // with NOZZLE_AS_PROBE this can be negative for a wider probing area.
